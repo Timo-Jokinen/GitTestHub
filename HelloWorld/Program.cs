3e94340bb2f5fb5e66ebbca2667bc2e73dece009
fitimjoki <fitimjoki@FINTIMJOKI1>
2016-10-21 10:09:19 +0300

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+            string minun_Oma_Nimi;
+            string Minun_Oma_Nimi;
+            Console.WriteLine("Hello World 6!");
+            heiKnokker();
+            HeiKnokker();
+            minun_Oma_Nimi = "timo";
+            Minun_Oma_Nimi = "Tomi";
+            string kolmas = minun_Oma_Nimi + " " + Minun_Oma_Nimi;
+            Console.ReadKey();
+        }
+
+        static void heiKnokker()
+        {
+
+        }
+
+        static void HeiKnokker() { }
+
+    }
+}
